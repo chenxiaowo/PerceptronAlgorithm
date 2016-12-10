1c2ae59c05c8d7ddb874543c274622abc39b4594
Jenkin <chenxiaowo@yeah.com>
2016-12-10 22:12:34 +0800

    init the project to git

---
+++ PerceptronAlgorithm/Perceptron.cpp
@@ -0,0 +1,12 @@
+#include "Perceptron.h"
+
+
+
+Perceptron::Perceptron()
+{
+}
+
+
+Perceptron::~Perceptron()
+{
+}
---
+++ PerceptronAlgorithm/Perceptron.h
@@ -0,0 +1,8 @@
+#pragma once
+class Perceptron
+{
+public:
+	Perceptron();
+	virtual ~Perceptron();
+};
+
