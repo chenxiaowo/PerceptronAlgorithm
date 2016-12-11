f83223b9be64b4d747b2dc28bbaa62eb8cb84ccd
建杰 史 <519430889@qq.com>
2016-12-11 15:54:09 +0800

    Finished symbolNormalization function. Initialize train function.

--- PerceptronAlgorithm/Perceptron.cpp
+++ PerceptronAlgorithm/Perceptron.cpp
@@ -11,6 +11,20 @@ Perceptron::~Perceptron()
 {
 }
 
+void Perceptron::train(const vector<vector<double> >& trainData, const vector<vector<int> >& response)
+{
+	if (weights.empty())
+	{
+		weights = vector<double>(trainData[0].size, 1);
+	}
+	x = trainData;
+	augmentation(x);
+	symbolNormalization(x, response);
+
+	//x为已增广化和符号规范化的训练数据,weights为权矢量
+
+}
+
 void Perceptron::augmentation(vector<vector<double> >& trainData)
 {
 	vector<vector<double> >::iterator itr;
@@ -22,7 +36,24 @@ void Perceptron::augmentation(vector<vector<double> >& trainData)
 
 void Perceptron::symbolNormalization(vector<vector<double>>& trainData, const vector<vector<int>>& response)
 {
-
+	vector<vector<double> >::iterator dataItr;
+	vector<vector<int> >::const_iterator respItr;
+	for (dataItr = trainData.begin(),respItr = response.begin(); dataItr != trainData.end()||respItr !=response.end(); dataItr++,respItr++)
+	{
+		vector<int>::const_iterator respInnerItr = respItr->begin();
+		int i;
+		for (i = 0; i < respItr->size()&& *respInnerItr++ != 1; i++)
+		{
+		}
+		if (i==1)		//当属于第二类时，将其符号乘以-1
+		{
+			vector<double>::iterator dataInnerItr;
+			for (dataInnerItr= dataItr->begin(); dataInnerItr < dataItr->end(); dataInnerItr++)
+			{
+				*dataInnerItr = 0 - *dataInnerItr;
+			}
+		}
+	}
 }
 
 double Perceptron::mult(const vector<double> &left, const vector<double> &right)
