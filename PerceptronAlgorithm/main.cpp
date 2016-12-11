58df7ebd2c9a7973c1c1b94de2bd6bf23597db93
建杰 史 <519430889@qq.com>
2016-12-11 18:20:11 +0800

    Finished readDataFromFile fuction.

--- PerceptronAlgorithm/main.cpp
+++ PerceptronAlgorithm/main.cpp
@@ -1,11 +1,51 @@
 #include <iostream>
+#include <fstream>
 #include "Perceptron.h"
 
 using namespace std;
 
+void readDataFromFile(string filePath, vector<vector<double> >& vecs, vector<int>& response);
+
 int main()
 {
-	Perceptron perceptron();
+	vector<vector<double> > trainData;
+	vector<int> response;
+
+	readDataFromFile("data.txt", trainData, response);
+	Perceptron perceptron = Perceptron();
+	perceptron.setItrNum(1);
+	perceptron.setLRate(1);
+	perceptron.train(trainData,response);
 	system("pause");
 	return 0;
+}
+
+void readDataFromFile(string filePath, vector<vector<double> >& vecs, vector<int>& response)
+{
+	ifstream in;
+	in.open(filePath);
+
+	if (!in)
+	{
+		cerr << "file open failed!" << endl;
+		exit(-1);
+	}
+	int i = 0;
+	response.clear();
+	while (in.peek() != '\n' && !in.eof())
+	{
+		vector<double> vec;
+		double data;
+		while (in.peek() != '\n' && in.peek() != ',' && !in.eof())
+		{
+			in >> data;
+			vec.push_back(data);
+		}
+		response.push_back(i);
+		vecs.push_back(vec);
+		if (in.get() == '\n')	//if read \n
+		{
+			i++;
+		}
+	}
 }
